d1a2b5d3d6e7e43547e2edb5e5f71b387291a644
ColonelParrot <65585002+ColonelParrot@users.noreply.github.com>
2025-01-31 09:44:13 -0500

    fix load delay

--- assets/index.js
+++ assets/index.js
@@ -8,32 +8,29 @@ window.scrollTo(0, 0);
 document.body.style.position = "fixed"
 document.body.style.overflowY = "scroll"
 
-$('#title1').animate({ opacity: 1 }, 2000);
-$(background).css('filter', 'brightness(1)')
+window.addEventListener("load", function () {
+    $('#title1').animate({ opacity: 1 }, 2000);
+    $(background).css('filter', 'brightness(1)')
 
-setTimeout(function () {
+    setTimeout(function () {
 
-    new TxtAnime("#title2", {
-        effect: 'txt-an-3',
-        repeat: false
-    })
+        new TxtAnime("#title2", {
+            effect: 'txt-an-3',
+            repeat: false
+        })
 
-    setTimeout(() => {
-        title2.style.visibility = "visible"
-    }, 100)
+        setTimeout(() => {
+            title2.style.visibility = "visible"
+        }, 100)
 
-    setTimeout(() => {
-        $(logo).animate({ opacity: 1 }, 1000);
+        setTimeout(() => {
+            $(logo).animate({ opacity: 1 }, 1000);
 
-        document.body.style.position = "unset"
-        document.body.style.overflowY = "auto"
-    }, 1500)
-}, 500)
-
-
-
-
-
+            document.body.style.position = "unset"
+            document.body.style.overflowY = "auto"
+        }, 1500)
+    }, 500)
+})
 
 AOS.init({
     startEvent: 'load',
